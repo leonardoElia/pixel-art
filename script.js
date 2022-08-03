5f779b15befa44bcb69a535b285e4275743e9dbd
Leonardo Elias Souza de Oliverira Meireles <leonardoelias802@gmail.com>
2022-08-03 18:51:34 -0300

    Fazendo quase 8 requesitos

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const divs = document.querySelectorAll('#pixel-board div'); // o lenght é 5
 for(let contadora =0; contadora < divs.length; contadora +=1){
     for(let cont=1; cont<=5; cont +=1){
     let quadrado = document.createElement('div');
-    quadrado.className = 'pixel';
-    quadrado.style.backgroundColor = 'white';
+    quadrado.classList.add('pixel');
+    quadrado.classList.add('corPixel');
     divs[contadora].appendChild(quadrado)
 }
 
@@ -15,11 +15,6 @@ for(let contadora =0; contadora < divs.length; contadora +=1){
 //Fazendo a cor preta ficar seleciionada
 const corPreta = document.getElementsByClassName('color')[0];
 corPreta.classList.add('selected');
-
-//Recuperando todas as cores
-const corRosa = document.getElementsByClassName('color')[1];
-const corAzul = document.getElementsByClassName('color')[2];
-const corVermelho = document.getElementsByClassName('color')[3];
 
 //Fazendo apenas um cor ficar com a classe selected
 let paletaDecores = document.getElementById('color-palette');
@@ -43,9 +38,30 @@ for(let index =0; index < cores.length; index +=1){
 
 paletaDecores.addEventListener('click', seleciona);
 
+//Fazendo pintar
+const pixels = document.getElementsByClassName('pixel');
 
-const divPaiPixels = document.getElementById('pixel-board');
 function pintar(event){
+    console.log('teste');
+    let todasCores = document.getElementsByClassName('color');
+    for(let cont=0; cont < todasCores.length; cont +=1){
+      let corAtual = todasCores[cont];
+      if(corAtual.classList.contains('selected')){
+       let corSelcionada = corAtual.classList.item(1);
+       event.target.classList.add(corSelcionada);
+       event.target.classList.remove('corPixel');
+
+       console.log(event.target);
+      }
+    }
+
 }
 
-divPaiPixels.addEventListener('click', pintar)
+
+for(let conta =0; conta < pixels.length; conta +=1){
+
+
+pixels[conta].addEventListener('click', pintar);
+
+}
+
